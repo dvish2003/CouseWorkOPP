ada5e4b761aa975ef0060b366839bd81d1d43907
D Vishan Chathuranga <Vishanchathuranga2003@gmail.com>
2024-03-18 20:36:43 +0530

    Initial commit oopGame

---
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep.service;
+
+public class AiPlayer extends Player {
+    public AiPlayer(Board board) {
+        super(board);
+    }
+
+    public void movePiece(int col) {
+        //;
+        do {
+            col = (int) ((Math.random() * 6));
+        } while (!board.isLegalMove(col));
+
+        board.updateMove(col, Piece.GREEN);
+        board.getBoardUI().update(col, false);
+
+        Winner winner = board.findWinner();
+        if (winner.getWinningPiece().equals(Piece.EMPTY)) {
+            if (!board.existLegalMoves()) {
+                board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
+            }
+        } else {
+            board.getBoardUI().notifyWinner(winner);
+        }
+    }
+}
